ef6fa1e12b61936fd4f5abb1aced6a7c727ed71d
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-02-15 14:29:46 -0800

    optimise to O(n) time (with additional memory)

--- src/main/java/com/leetcode/quiram/twosum/Solution.java
+++ src/main/java/com/leetcode/quiram/twosum/Solution.java
@@ -1,15 +1,23 @@
 package com.leetcode.quiram.twosum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /***
  * <a href="https://leetcode.com/problems/two-sum/">source</a>
  */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        final Map<Integer, Integer> indexLookUp = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            indexLookUp.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            final Integer matchingIndex = indexLookUp.get(target - nums[i]);
+            if (matchingIndex != null && i != matchingIndex) {
+                return new int[]{i, matchingIndex};
             }
         }
 
