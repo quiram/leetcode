847ff6c9266516310130c0400adaa95d547e913a
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-02-21 16:27:21 -0800

    add an early finish as soon as one duplicate is found in "217. Contains Duplicate"

--- src/main/java/com/leetcode/quiram/containsduplicate/Solution.java
+++ src/main/java/com/leetcode/quiram/containsduplicate/Solution.java
@@ -1,18 +1,20 @@
 package com.leetcode.quiram.containsduplicate;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * <a href="https://leetcode.com/problems/contains-duplicate/description/">217. Contains Duplicate</a>
  */
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        final Set<Integer> set = Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.toSet());
+        final Set<Integer> set = new HashSet<>();
 
-        return set.size() != nums.length;
+        int i;
+        for (i = 0; i < nums.length && !set.contains(nums[i]); i++) {
+            set.add(nums[i]);
+        }
+
+        return i != nums.length;
     }
 }
