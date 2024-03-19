5aa562766515979203478accdd2a1ede16a31f74
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-19 15:50:48 -0700

    simplify solution for "Maximum Subarray" using Kadane's algorithm
    My version was pretty close to Kadane's, anyway

--- src/main/java/com/leetcode/quiram/medium/maximumsubarray/Solution.java
+++ src/main/java/com/leetcode/quiram/medium/maximumsubarray/Solution.java
@@ -8,24 +8,12 @@ import static java.lang.Integer.MIN_VALUE;
 class Solution {
     public int maxSubArray(int[] nums) {
         int max = MIN_VALUE;
-        int acc = MIN_VALUE;
+        int acc = 0;
 
         for (int n : nums) {
-            int accPlusN = acc == MIN_VALUE && n < 0 ? MIN_VALUE : acc + n;
-            if (acc > n && acc > accPlusN) {
-                if (acc > max) {
-                    max = acc;
-                }
-                acc = accPlusN;
-            } else if (n >= acc && n >= accPlusN) {
-                acc = n;
-            } else {
-                acc = accPlusN;
-            }
-        }
-
-        if (acc > max) {
-            max = acc;
+            int accPlusN = acc + n;
+            acc = Integer.max(n, accPlusN);
+            max = Integer.max(max, acc);
         }
 
         return max;
