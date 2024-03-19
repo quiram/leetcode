4da44006f5404ff6f30a3ae555d3aaaa6693ea93
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-19 14:15:16 -0700

    improve "219. Contains Duplicate II" to linear solution

--- src/main/java/com/leetcode/quiram/containsduplicate2/Solution.java
+++ src/main/java/com/leetcode/quiram/containsduplicate2/Solution.java
@@ -1,15 +1,24 @@
 package com.leetcode.quiram.containsduplicate2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/contains-duplicate-ii/description/">219. Contains Duplicate II</a>
  */
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        final Map<Integer, Integer> m = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+            Integer j = m.get(nums[i]);
+            if (j == null) {
+                m.put(nums[i], i);
+            } else {
+                if (i - j <= k) {
                     return true;
                 }
+                m.put(nums[i], i);
             }
         }
 
